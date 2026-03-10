7e2c75971d15df7a8a966f258030a6d0176496ad
Non <Nonhaha49@gmail.com>
2026-03-10 19:51:12 +0700

    remove unused file

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template, request,
+﻿from flask import (Blueprint, render_template, request,
                     flash, redirect, session, url_for)
 import sqlite3
 
@@ -7,7 +7,7 @@ dash = Blueprint("dash", __name__)
 @dash.route("/")
 @dash.route("/dashboard")
 def dashboard():
-    if not session.get("user_id"):
+    if "user_id" not in session:
         flash("Login first!", category="error")
         return redirect(url_for("auth.login"))
     
@@ -15,18 +15,56 @@ def dashboard():
     cur = conn.cursor()
 
     cur.execute(
-        "SELECT amount, type, description FROM transactions WHERE user_id=?",(session["user_id"],)
+        "SELECT id, amount, type, description FROM transactions WHERE user_id=?",(session["user_id"],)
     )
     
     transactions = cur.fetchall()
 
     balance = 0
     for t in transactions:
-        if t[1] == "income":
-            balance += t[0]
+        if t[2] == "income":
+            balance += t[1]
         else:
-            balance -= t[0]
+            balance -= t[1]
+
+    cur.execute("""
+    SELECT type, SUM(amount)
+    FROM transactions
+    WHERE user_id=?
+    GROUP BY type
+    """, (session["user_id"],))
+
+    data = cur.fetchall()
+
+    income = 0
+    expense = 0
+
+    for d in data:
+        if d[0] == "income":
+            income = d[1]
+        else:
+            expense = d[1]
 
     conn.close()
 
-    return render_template("dashboard.html", transactions=transactions, balance=balance)
+    return render_template("dashboard.html", transactions=transactions, balance=balance,
+                           income=income, expense=expense)
+
+@dash.route("/delete/<int:id>", methods=["POST"])
+def delete(id):
+    if "user_id" not in session:
+        flash("Login first!", category="error")
+        return redirect(url_for("auth.login"))
+
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM transactions WHERE id=? AND user_id=?",
+        (id, session["user_id"])
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("dash.dashboard"))
